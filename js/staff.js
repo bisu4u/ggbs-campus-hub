15fb622f3b9f063c0c154c7186b437b42334b716
bisu4u <smigofc@gmail.com>
2026-02-14 11:22:39 +0530

    empty commit message

--- js/staff.js
+++ js/staff.js
@@ -7,7 +7,7 @@ name:"Mrs. Sujata Pattnaik",
 designation:"Principal",
 email:"principal@ggbs.edu",
 contact:"9437137765",
-exp:"5 Years",
+exp:"20 Years",
 img:"https://lh3.googleusercontent.com/gps-cs/AOGcYQSXl9AaL-HBcBQma11t1l-XGDfq1IZBTs7JRXUS3J-GqX4QVBESjJXXdtRcRXIzGRK1ZWtkevKRt8a8ufINUt7Hwh0wU2GD_56WAziTUKE2Y1M9fKbIF3ZjW3y-U6CPG4L0IDRclb-n_mM=w600-h988-p-k-no"
 }
 ];
@@ -18,7 +18,7 @@ name:"Mr. Chittaranjan Mahapatra",
 designation:"Head of Department",
 email:"chitta@ggbs.edu",
 contact:"9348328277",
-exp:"1 Years",
+exp:"4 Years",
 img:"https://lh3.googleusercontent.com/gps-cs/AOGcYQS49mNCVnvTO6eaakwTuWzoJpBsFfUj3XkbUICUifjfi3b_vEacz8PaVlTQxcvCA1RYaBsgat_iN0Kjs6can5xaKtDXhkvcC4ukxC_NxEPCZF8853_VmCm2Z-OCQoDtCsKo-VgsErHspj9s=w600-h988-p-k-no"
 },
 {
@@ -26,7 +26,7 @@ name:"Mr. Pradyumna Mishra",
 designation:"Coordinator",
 email:"pradyumna@ggbs.edu",
 contact:"9938228376",
-exp:"1 Years",
+exp:"2 Years",
 img:"https://lh3.googleusercontent.com/gps-cs/AOGcYQQMw1dNKmax1dz2JFg7jLJWy0w4QiKZh15KEiA8wgRo0B1Xgfr_xIX1iN182qK6_CBGb5zJdna1yZ_5Wt_VrRXCr35gVussPbtBtaotQXILD4Lz7U28G7aWUdzf8mjoBQLSPz4tepcCOGpQ=w600-h988-p-k-no"
 },
 {
@@ -34,7 +34,7 @@ name:"Mr. Subham Panda",
 designation:"Lecturer",
 email:"subham@ggbs.edu",
 contact:"9438449205",
-exp:"1 Years",
+exp:"2 Years",
 img:"https://lh3.googleusercontent.com/gps-cs/AOGcYQQxUmSzAnGEs1gsVsWkxr2Z92CBnLToxVwgbBkf2395UK1pg9QzdCiPy1pELIKartFTHFifH-iguv6h6drby46vOb0MeumIImpyIqnn8gK6R5ho59UM5wGE2ul-6Z7OASzcsX8_vFKi-J8q=w600-h988-p-k-no"
 },
 {
@@ -42,7 +42,7 @@ name:"Ms. Sabana Pattnaik",
 designation:"Lecturer",
 email:"sabana@ggbs.edu",
 contact:"8763517818",
-exp:"1 Years",
+exp:"2 Years",
 img:"https://lh3.googleusercontent.com/gps-cs/AOGcYQQGjcI7IrqGfPAca1hBXVPtXaU33KbD5DxX2JEYanOIp8bEZ2q7J4-5ByY6vHefJj45mzVgQLWfSUmiYxJyPGxYGO3aZEtGKmdeu4GhBd4IIvoSnKX24a7rkfY1x0veK0JkoI0va1usTv11=w600-h988-p-k-no"
 },
 {
@@ -50,7 +50,7 @@ name:"Ms. lovlin Pradhan",
 designation:"Lecturer",
 email:"lovlin@ggbs.edu",
 contact:"8018580753",
-exp:"1 Years",
+exp:"3 Years",
 img:"https://lh3.googleusercontent.com/gps-cs/AOGcYQSGG4mvVIWoZ_QBLFlJPcUWdw-X-xpEbCt4Ub0yixy_2zgD_2hIM_3__VpKabauio2o4tEwg3dKQmc24y93napdljyvwfEH3uPVnOcjl9s4oMqliGhc_Ud8Q8WdmyaeEJTVUZjKP_1AH3jf=w600-h988-p-k-no"
 },
 {
@@ -58,7 +58,7 @@ name:"Mr. Deepak Kumar sahu",
 designation:"Lecturer",
 email:"deepak@ggbs.edu",
 contact:"6371233500",
-exp:"1 Years",
+exp:"2 Years",
 img:"https://lh3.googleusercontent.com/gps-cs/AOGcYQTA8n7RP5WYl2lt3L2A4AoJh18oEXMFGwQXF747J8boDlue6HUl2W6_N-WhVgG6dz-agXDueX1uyD_5YfVLz7P2Yf3H6i4Kk4gxVq4K-mZcReDzlD90O0vP9uHZ3FytO2EtQyrBf1agQm04=w600-h988-p-k-no"
 }
 ];
@@ -69,7 +69,7 @@ name:"Ms. Manaswini Dash",
 designation:"Head Of Department",
 email:"manaswini@ggbs.edu",
 contact:"9348824478",
-exp:"1 Years",
+exp:"5 Years",
 img:"https://lh3.googleusercontent.com/gps-cs/AOGcYQRK4dbv0eQ3BUDWWmmXPP8ENO7xt6XM0U6Um1qNN9IjlVVWxfsuQeX0Uod8CjSgFHv2ki5XexfcL4jGCwCn8NB62ILVlJu_HOWb4heu0tgio2ePjnXnjE5RWxYbhAkKc2bzEXBEhR0IN3kf=w600-h988-p-k-no"
 },
 {
@@ -77,7 +77,7 @@ name:"Ms. Sabana Pattnaik",
 designation:"Lecturer",
 email:"sabana@ggbs.edu",
 contact:"8763517818",
-exp:"1 Years",
+exp:"2 Years",
 img:"https://lh3.googleusercontent.com/gps-cs/AOGcYQQGjcI7IrqGfPAca1hBXVPtXaU33KbD5DxX2JEYanOIp8bEZ2q7J4-5ByY6vHefJj45mzVgQLWfSUmiYxJyPGxYGO3aZEtGKmdeu4GhBd4IIvoSnKX24a7rkfY1x0veK0JkoI0va1usTv11=w600-h988-p-k-no"
 },
 {
@@ -85,7 +85,7 @@ name:"Ms. Srusti Shagun Swain",
 designation:"Lecturer",
 email:"srusti@ggbs.edu",
 contact:"6371223993",
-exp:"1 Years",
+exp:"2 Years",
 img:"https://lh3.googleusercontent.com/gps-cs/AOGcYQQd9chUu98kxoUIrFaPWJQMmxR2i-FyhljfLgl7wCcW6cizUgkQSrSHddxAQKuNK8kIpJlGW8zWBezQqtjIZWggmopq55ayKh-9QlCdKfBx3AZV7FabP08iPjCFqN_6VKvoQ0SgYgEi-Abp=w600-h988-p-k-no"
 }
 ];
@@ -96,7 +96,7 @@ name:"Mr. Chittaranjan Mahapatra",
 designation:"Head of Department",
 email:"chitta@ggbs.edu",
 contact:"9348328277",
-exp:"2 Years",
+exp:"5 Years",
 img:"https://lh3.googleusercontent.com/gps-cs/AOGcYQS49mNCVnvTO6eaakwTuWzoJpBsFfUj3XkbUICUifjfi3b_vEacz8PaVlTQxcvCA1RYaBsgat_iN0Kjs6can5xaKtDXhkvcC4ukxC_NxEPCZF8853_VmCm2Z-OCQoDtCsKo-VgsErHspj9s=w600-h988-p-k-no"
 },
 {
@@ -104,7 +104,7 @@ name:"Mr. Pradyumna Mishra",
 designation:"Coordinator",
 email:"pradyumna@ggbs.edu",
 contact:"9938228376",
-exp:"1 Years",
+exp:"2 Years",
 img:"https://lh3.googleusercontent.com/gps-cs/AOGcYQQMw1dNKmax1dz2JFg7jLJWy0w4QiKZh15KEiA8wgRo0B1Xgfr_xIX1iN182qK6_CBGb5zJdna1yZ_5Wt_VrRXCr35gVussPbtBtaotQXILD4Lz7U28G7aWUdzf8mjoBQLSPz4tepcCOGpQ=w600-h988-p-k-no"
 },
 {
@@ -112,7 +112,7 @@ name:"Mr. Subham Panda",
 designation:"Lecturer",
 email:"subham@ggbs.edu",
 contact:"9438449205",
-exp:"1 Years",
+exp:"2 Years",
 img:"https://lh3.googleusercontent.com/gps-cs/AOGcYQQxUmSzAnGEs1gsVsWkxr2Z92CBnLToxVwgbBkf2395UK1pg9QzdCiPy1pELIKartFTHFifH-iguv6h6drby46vOb0MeumIImpyIqnn8gK6R5ho59UM5wGE2ul-6Z7OASzcsX8_vFKi-J8q=w600-h988-p-k-no"
 },
 {
@@ -120,7 +120,7 @@ name:"Ms. lovlin Pradhan",
 designation:"Lecturer",
 email:"lovlin@ggbs.edu",
 contact:"8018580753",
-exp:"1 Years",
+exp:"2 Years",
 img:"https://lh3.googleusercontent.com/gps-cs/AOGcYQSGG4mvVIWoZ_QBLFlJPcUWdw-X-xpEbCt4Ub0yixy_2zgD_2hIM_3__VpKabauio2o4tEwg3dKQmc24y93napdljyvwfEH3uPVnOcjl9s4oMqliGhc_Ud8Q8WdmyaeEJTVUZjKP_1AH3jf=w600-h988-p-k-no"
 }
 ];
